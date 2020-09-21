c31ceb692f078740bb0f27bcb43070392c1f2558
Tony Skinny <jimenaalvarez.tony@gmail.com>
2020-09-21 13:56:09 +0200

    upload build master

---
+++ precache-manifest.1e3d64a6b1c59965da672688f2c02f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5012b6d5e3e439e5d104525d06b7936f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "646b1a26e37c0eb11510",
+    "url": "/static/css/main.44637c54.chunk.css"
+  },
+  {
+    "revision": "370836e14d161b1ccda5",
+    "url": "/static/js/2.9c03e4a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c03e4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646b1a26e37c0eb11510",
+    "url": "/static/js/main.93a24f04.chunk.js"
+  },
+  {
+    "revision": "10ab1aa669f2a529a659",
+    "url": "/static/js/runtime-main.7124e8d1.js"
+  },
+  {
+    "revision": "35ac622873cef13ed424c99a011ca786",
+    "url": "/static/media/logo-skinner-fondo.35ac6228.png"
+  }
+]);
