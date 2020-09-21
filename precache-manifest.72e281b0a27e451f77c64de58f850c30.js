6a0713b43650218df6f55689438bbc907122e9ae
Tony Skinny <jimenaalvarez.tony@gmail.com>
2020-09-22 00:13:58 +0200

    main

---
+++ precache-manifest.72e281b0a27e451f77c64de58f850c30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cae390507b6be65205eb26c50ad0125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caf2c7491cf414af6a0e",
+    "url": "/static/css/main.4ebb5d84.chunk.css"
+  },
+  {
+    "revision": "370836e14d161b1ccda5",
+    "url": "/static/js/2.9c03e4a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c03e4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf2c7491cf414af6a0e",
+    "url": "/static/js/main.b9324915.chunk.js"
+  },
+  {
+    "revision": "10ab1aa669f2a529a659",
+    "url": "/static/js/runtime-main.7124e8d1.js"
+  },
+  {
+    "revision": "a7803ddf902b6c7b5ac326af14e450b3",
+    "url": "/static/media/FugazOne-Regular.a7803ddf.ttf"
+  },
+  {
+    "revision": "a1b508542605c36a1a69391596b56c79",
+    "url": "/static/media/logo-skinner-fondo.a1b50854.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff00a51ba671c383f037e0c0f6f43694.js"
+  "/precache-manifest.72e281b0a27e451f77c64de58f850c30.js"
 );
 
 self.addEventListener('message', (event) => {
